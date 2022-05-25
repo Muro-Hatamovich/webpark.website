8d4dd1457a1c080fb304cd012e7337bb1e933234
Muro-Hatamovich <hatamovich.info@gmail.com>
2022-05-26 01:25:17 +0500

    added hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+
 // Importing components
 import Logo from "./Logo";
 import Navbar from "./Navbar";
+import Hamburger from "hamburger-react";
 
-const Header = () => (
-	<header className="header">
-		<div className="header__container container">
-			<Logo className="header__logo logo" src="/images/logo-dark.png" />
-			<Navbar className="navbar" />
-			<button className="header__btn">Оставить заявку</button>
-		</div>
-	</header>
-);
+const Header = () => {
+	const [isMobileNav, setIsMobileNav] = useState(false);
+
+	return (
+		<header className="header">
+			<div className="header__container container">
+				<Logo
+					className="header__logo logo"
+					src="/images/logo-dark.png"
+				/>
+				<Navbar className={isMobileNav ? "active navbar" : "navbar"} />
+				<button className="header__btn">Оставить заявку</button>
+				<Hamburger toggled={isMobileNav} toggle={setIsMobileNav} />
+			</div>
+		</header>
+	);
+};
 
 export default Header;
